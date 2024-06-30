a41601a4e19c2d316cd808a3986c81593b7dd616
DepleyPuma <szymon.szarek1@wp.pl>
2024-06-30 16:32:19 +0200

    logic for contact section

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,14 @@ const navMobileAllLinks = navMobile.querySelectorAll('a');
 const navMobileAllLiItems = document.querySelectorAll('.nav-mobile__list--item');
 const navCloseBtn = document.querySelector('.nav-mobile__close-btn');
 const navBurgerBtn = document.querySelector('.nav-mobile__menu');
+const consultationBtn = document.querySelector('#consultations');
+const personalTrainingOnlineBtn = document.querySelector('#online');
+const personalTrainingBtn = document.querySelector('#personalTraining');
+const contactSection = document.querySelector('.contact');
+const selectOptionInput = document.querySelector('#selectedOption');
+const optionsArr = [...document.querySelectorAll('option')].map(option => option.value);
+
+let option;
 
 const openNav = () => {
 	let delay = 0;
@@ -23,6 +31,26 @@ const closeNav = () => {
 		link.classList.remove('nav-items-animation');
 	});
 };
+
+const setOption = selectedOption => {
+	const index = optionsArr.indexOf(selectedOption);
+	option = selectedOption;
+	selectOptionInput.selectedIndex = index;
+
+	const scrollToContactSection = contactSection.offsetTop;
+
+	selectOptionInput.focus();
+
+	window.scrollTo(0, scrollToContactSection);
+};
+
+consultationBtn.addEventListener('click', () => setOption(optionsArr[1]));
+personalTrainingOnlineBtn.addEventListener('click', () => setOption(optionsArr[2]));
+personalTrainingBtn.addEventListener('click', () => setOption(optionsArr[3]));
+
+selectOptionInput.addEventListener('change', e => {
+	option = e.currentTarget.value;
+});
 
 navMobileAllLinks.forEach(link => link.addEventListener('click', closeNav));
 navBurgerBtn.addEventListener('click', openNav);
